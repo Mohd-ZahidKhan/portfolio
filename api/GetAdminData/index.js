3d2f1501af6d96668ce0eea7d4ea47a92f0af6e8
Mohd-ZahidKhan <rjczahidkhan12com706@gmail.com>
2026-01-14 16:10:45 +0530

    version 1.1.3 added Admin Page

--- api/GetAdminData/index.js
+++ api/GetAdminData/index.js
@@ -1,28 +1,36 @@
 module.exports = async function (context, req, allRecords) {
-
     const ADMIN_SECRET = process.env.ADMIN_PASSWORD; 
-
     const userKey = req.query.key;
 
-    if (!ADMIN_SECRET) {
-        context.res = { status: 500, body: "Server Error: Admin configuration missing." };
-        return;
-    }
+    const corsHeaders = {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+    };
 
-    if (userKey !== ADMIN_SECRET) {
-        context.res = {
-            status: 401, 
-            body: { message: "Access Denied: Wrong Key!! Please leave this Page Immediately your not authorized to visit this Page!!" }
+   
+    if (!ADMIN_SECRET) {
+        context.res = { 
+            status: 500, 
+            headers: corsHeaders, 
+            body: "Server Error: Admin configuration missing." 
         };
         return;
     }
 
+  
+    if (userKey !== ADMIN_SECRET) {
+        context.res = {
+            status: 401, 
+            headers: corsHeaders, 
+            body: { message: "⛔ Access Denied: Wrong Key" }
+        };
+        return;
+    }
+
+    
     context.res = {
         status: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        },
+        headers: corsHeaders,
         body: allRecords
     };
 }
